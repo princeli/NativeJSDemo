5a48343b2b34c8db649d0bbd264e482013e02201
princeli <princeli@live.cn>
2015-12-10 15:16:46 +0800

    Initial Commit

---
+++ NativeJSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NativeJSDemo
+//
+//  Created by ly on 15/12/10.
+//  Copyright © 2015年 princeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NativeJSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NativeJSDemo
+//
+//  Created by ly on 15/12/10.
+//  Copyright © 2015年 princeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
