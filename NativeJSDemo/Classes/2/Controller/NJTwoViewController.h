e65d4fc4179ef08284b2a0bc2e3213d654ea3644
princeli <princeli@live.cn>
2015-12-12 14:12:18 +0800

    add js support

--- NativeJSDemo/Classes/2/Controller/NJTwoViewController.h
+++ NativeJSDemo/Classes/2/Controller/NJTwoViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface NJTwoViewController : UITableViewController
+@interface NJTwoViewController : UIViewController<UIWebViewDelegate>
 
 @end
--- NativeJSDemo/Classes/2/Controller/NJTwoViewController.m
+++ NativeJSDemo/Classes/2/Controller/NJTwoViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "NJTwoViewController.h"
+#import "WebViewJavaScriptBridge.h"
 
 @interface NJTwoViewController ()
-
+@property WebViewJavascriptBridge *bridge;
 @end
 
 @implementation NJTwoViewController
@@ -17,6 +18,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    // 创建UIWebView对象
+    CGRect frame = CGRectMake(self.view.bounds.origin.x, self.view.bounds.origin.y, self.view.bounds.size.width, self.view.bounds.size.height-44);
+    UIWebView * webView = [[UIWebView alloc] initWithFrame:frame];
+    [self.view addSubview:webView];
+    // 配置logging
+    [WebViewJavascriptBridge enableLogging];
+    
+    // 创建WebViewJavascriptBridge对象并与UIWebView对象绑定
+    self.bridge = [WebViewJavascriptBridge bridgeForWebView:webView handler:^(id data, WVJBResponseCallback responseCallback) {
+        NSLog(@"Received message from javascript: %@", data);
+        responseCallback(@"Right back atcha");
+    }];
+    
+    [self loadExamplePage:webView];
+}
+
+- (void)loadExamplePage:(UIWebView*)webView {
+    NSURL *baseURL =[[NSURL alloc] initWithString:@"http://yyxx.10086.cn/v.html"];
+    [webView loadRequest:[NSURLRequest requestWithURL:baseURL]];
+    //[webView loadHTMLString:appHtml baseURL:baseURL];
 }
 
 - (void)didReceiveMemoryWarning {
